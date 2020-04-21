f76351caa3cce8201ab85037c230d14f6088c2e9
Simon Chen <simonhchen@gmail.com>
2020-04-21 14:04:23 -0700

    add data_collection script and database script

---
+++ data_collection.py
@@ -0,0 +1,26 @@
+# Collects daily COVID-19 cases from John Hopkins Repo
+
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+import pandas as pd
+
+base = "https://raw.githubusercontent.com"
+url = requests.get(
+    'https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports').text
+soup = BeautifulSoup(url, features="html.parser")
+
+data_dict = {}
+
+for link in (urljoin(base, a["href"]) for a in soup.select("a[href$='.csv']")):
+    link = link.replace('/blob', '')
+    #  Stores each daily COVID-19 .csv file in John Hopkins repo as a dataframe in a dictionary
+    data_dict[link.replace(
+        "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/",
+        "")] = pd.read_csv(link)
+
+for value in data_dict.values():
+    print(value)
+
+
